8195bd64a189c25309a6987dc42e04ab2c9057bd
Dusty McCord <Dustymccord@gmail.com>
2021-04-19 21:43:26 -0700

    setup index

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,19 +8,7 @@ export default function Home() {
         <title>Create Next TypeScript App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <h1>KPARTY</h1>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   );
 }
