591eb6cf6e7f424931082272c49ba5ca056edd5c
Dusty McCord <Dustymccord@gmail.com>
2021-08-10 22:30:40 -0700

    Remove ;

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -1,6 +1,6 @@
-import { initializeApp } from "firebase-admin";
-import firebase from "firebase/app";
-import "firebase/auth";
+import { initializeApp } from "firebase-admin"
+import firebase from "firebase/app"
+import "firebase/auth"
 
 const config = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
@@ -11,16 +11,16 @@ const config = {
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING,
   appId: process.env.NEXT_PUBLIC_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
-};
+}
 
 function initializeFirebase(keys) {
   if (!firebase.apps.length) {
-    return firebase.initializeApp(keys);
+    return firebase.initializeApp(keys)
   }
-  return firebase;
+  return firebase
 }
 
-const app = initializeFirebase(config);
+const app = initializeFirebase(config)
 
-export const auth = app.auth();
-export default app;
+export const auth = app.auth()
+export default app
