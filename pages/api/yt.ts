b6b819cd519b12f755dbf621dfb11b47d02bc370
Dusty <dustymccord@gmail.com>
2023-05-29 17:40:01 -0700

    Move new client inside of function (#21)

--- pages/api/yt.ts
+++ pages/api/yt.ts
@@ -3,25 +3,23 @@ import { IVideoData } from '../../utils/index'
 import { Client } from "youtubei";
 
 
-const youtube = new Client();
 
 export default async (req, res) => {
 
   if (req.method === "GET") {
+    const youtube = new Client();
 
     const { song } = req.query;
 
     const results = await youtube.search(`${song} karaoke`, { type: "video" })
 
     if (!!results.items.length) {
-
       const resultsConformed: IVideoData[] = results.items.map((vid) => {
         const { duration, id, title, channel } = vid
         return { artist: channel?.name || 'no artist info', duration: duration || 0, id, original_title: title, publishedAt: '', title }
       })
-
       res.status(200).json(resultsConformed);
     }
-
+    res.status(403).json({ message: "Bad request" })
   }
 };
